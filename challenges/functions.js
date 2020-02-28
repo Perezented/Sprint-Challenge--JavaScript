d12bb06a15be8bd301cd38bcfba7660b241abff9
Perezented <57721734+Perezented@users.noreply.github.com>
2020-02-28 10:45:19 -0600

    Added dinos

--- challenges/functions.js
+++ challenges/functions.js
@@ -38,7 +38,7 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: closure.
+// Explanation: closure. Read the README please, question 3
 
 const external = "I'm outside the function";
 
